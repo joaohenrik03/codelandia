7b87b681e5f10ec404273c7ccc7bb7ac8a634a0e
João Henrik <jh.signori@gmail.com>
2022-07-14 21:58:37 -0300

    Updated content and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { LogoCodelandia } from "./components/LogoCodelandia";
+
 export function App() {
   return (
-    <>
-        <p> teste</p>
-    </>
+    <div className="w-[90%] mx-auto">
+        <header className="mt-6 flex flex-col items-center justify-center gap-12">
+          <LogoCodelandia />
+          <a href="#" className="inline-block w-[100%] py-5 border border-purple-500 rounded-md font-sans text-center text-gray-50 text-base uppercase">
+            Entrar na comunidade
+          </a>
+        </header>
+    </div>
   )
 }
