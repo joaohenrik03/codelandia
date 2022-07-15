344939fcf1e741d5430aedafe4fa4a6a96a79ea9
João Henrik <jh.signori@gmail.com>
2022-07-14 21:38:46 -0300

    Fonts and colors customized installed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['DM Sans', 'sans-serif'],
+        'plex': ['IBM Plex Mono', 'monospace']
+      },
+      colors: {
+        'purple-500': '#873EFF',
+        'green-400': '#04D361',
+        'gray-300': '#AEAEAE', 
+        'gray-50': '#F9F9F9',
+      }
+    },
   },
   plugins: [],
 }
