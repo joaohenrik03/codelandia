88019940aa547c64c99c391c6cb82feccd66406f
João Henrik <jh.signori@gmail.com>
2022-07-15 15:42:44 -0300

    Updated header and footer styling for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import { DesignCard } from "./components/DesignCard";
 import { KeyAndParenthesesIcon } from "./components/KeyAndParenthesesIcon";
 import { LogoCodelandia } from "./components/LogoCodelandia";
 
-export function App() {
+export function App() { //md
   return (
     <div className="w-[90%] mx-auto pb-36">
-        <header className="my-6 flex flex-col items-center justify-center gap-12">
+        <header className="mt-6 mb-12 md:mt-12 md:mb-24 flex flex-col items-center gap-12 md:flex-row justify-between">
           <LogoCodelandia />
-          <a href="#" className="inline-block w-[100%] py-5 border border-purple-500 rounded-md font-sans text-center text-gray-50 uppercase">
-            Entrar na comunidade
-          </a>
+          <nav className="w-full md:w-auto gap-16 flex items-center justify-between">
+            <a href="#" className="hidden text-gray-50 uppercase md:inline-block">
+              Meu GitHub
+            </a>
+            <a href="#" className="inline-block w-[100%] md:w-64 py-5 border border-purple-500 rounded-md font-sans text-center text-gray-50 uppercase">
+              Entrar na comunidade
+            </a>
+          </nav>
         </header>
 
         <main>
           <header className="mb-7">
-            <span className="font-plex text-green-400 text-sm font-bold tracking-wider uppercase mb-2">
+            <span className="font-plex text-green-400 text-md font-bold tracking-wider uppercase mb-2">
               Comunidade dev
             </span>
             <h1 className="font-plex text-left text-2xl font-semibold text-gray-50 mb-4">
@@ -37,11 +42,11 @@ export function App() {
           <i className="flex justify-center mb-6">
             <KeyAndParenthesesIcon />  
           </i>
-          <div className="w-full flex gap-8 mx-auto">
-            <span className="text-gray-300 font-plex text-sm">
+          <div className="w-full flex justify-center gap-8 mx-auto">
+            <span className="text-gray-300 font-plex text-md">
               © 2022 - João Henrik
             </span>
-            <span className="text-gray-300 font-plex text-sm">
+            <span className="text-gray-300 font-plex text-md">
               Codelândia
             </span>
           </div>
