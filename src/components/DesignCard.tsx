2570737315e3cee2f43c9b4bbfbbb5c61e0757f8
João Henrik <jh.signori@gmail.com>
2022-07-15 00:15:21 -0300

    Updated DesignCard Content

--- src/components/DesignCard.tsx
+++ src/components/DesignCard.tsx
@@ -1,7 +1,22 @@
 export function DesignCard() {
     return(
         <div>
-            Projeto
+            <img  
+                src="https://source.unsplash.com/random" 
+                alt="" 
+            />
+
+            <h2>
+                Nome do projeto 1
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis ligula, accumsan nec cursus in, eleifend sit amet dui.
+            </p>
+
+            <div>
+                <a href="#">Demo</a>
+                <a href="#">GitHub</a>
+            </div>
         </div>
     )
 }
