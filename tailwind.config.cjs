4a2c090ffddc70da70ce5918bc83cb768963174e
João Henrik <jh.signori@gmail.com>
2022-07-18 18:45:40 -0300

    Configured breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,25 @@ module.exports = {
         'stone-900': '#0E091B',
       }
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'smmd': '520px',
+      // => @media (min-width: 520px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 }
